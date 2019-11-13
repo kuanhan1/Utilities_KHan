d44e98ec1c339f942ec9a3979c90ba04eaa41dc2
KHan <han424@purdue.edu>
2019-11-13 14:45:22 -0500

    Update drive_update_cuda.sh

--- drive_update_cuda.sh
+++ drive_update_cuda.sh
@@ -1,9 +1,8 @@
 mkdir tempfolder
 cd tempfolder
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
+wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
 chmod +x cuda_9.0.176_384.81_linux-run
-# ./cuda_9.0.176_384.81_linux-run --extract=$HOME
-sudo ./cuda_9.0.176_384.81_linux-run
+sudo sh ./cuda_9.0.176_384.81_linux.run
 sudo bash -c "echo /usr/local/cuda/lib64/ > /etc/ld.so.conf.d/cuda.conf"
 sudo ldconfig
 cd ..
